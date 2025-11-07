688a0be424cca82417a20b4c91001f436498aa41
Sudin Kominfotik JB 04 <graficajati339@gmail.com>
2025-11-07 20:41:26 +0700

    UTS_create page splash screen awal

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uts_1125170135/screen/splash_screen_1.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
       ),
       debugShowCheckedModeBanner: false,
-      
+      home: MySplashScreen(),
     );
   }
 }
