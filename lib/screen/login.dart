96cebf4a769438b0a3da82a5dd025ceb1b19265b
Sudin Kominfotik JB 04 <graficajati339@gmail.com>
2025-11-08 21:48:11 +0700

    UTS_mengubah posisi menjadi center

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -7,41 +7,55 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              SizedBox(height: 10),
-              Icon(
-                Icons.lock_outline,
-                size: 80,
-                color: Colors.pink,
-              ),
-              SizedBox(height: 10),
-              Text("Selamat Datang",
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-              ),),
-              SizedBox(height: 10),
-              Text("Silahkan Login",
-              style: TextStyle(
-                fontSize: 15.0,
-                fontWeight: FontWeight.normal,
-              ),),
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  labelText: "Email",
-                  hintText: "Masukan email anda",
-                  prefixIcon: Icon(Icons.email_outlined),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                  )
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                SizedBox(height: 10),
+                Icon(
+                  Icons.lock_outline,
+                  size: 80,
+                  color: Colors.pink,
                 ),
-              )
-            ],
+                SizedBox(height: 10),
+                Text("Selamat Datang",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),),
+                SizedBox(height: 10),
+                Text("Silahkan Login",
+                style: TextStyle(
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.normal,
+                ),),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    hintText: "Masukan email anda",
+                    prefixIcon: Icon(Icons.email_outlined),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: BorderSide(color: Colors.pink[300]!),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: BorderSide(
+                        color: Colors.pink[700]!,
+                        width: 2,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox()
+              ],
+            ),
           ),
         ),
       ),
